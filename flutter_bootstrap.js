4f5ea20f822b317ba34d94f8962e5fa6892f3e23
woosikk <woosikk@users.noreply.github.com>
2025-04-21 12:09:13 +0000

    deploy: 015672f0eb0bded31bed27a345d85c31b515d96b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2682358033"
+    serviceWorkerVersion: "2055220838"
   }
 });
